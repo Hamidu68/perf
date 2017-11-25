33b4c597e2e6441250a84492e08cfb525f751bc1
hamid <hnejatol@uci.edu>
2017-11-25 15:49:32 -0800

    Revert "some minor changes"
    This reverts commit 4c5a458e1bc2eaaf3933f5a11b925bbebc1295a0.

--- run.py
+++ run.py
@@ -9,6 +9,7 @@ os.system("rm -r "+ Directory_Save_Plots)
 os.system("mkdir "+ Directory_Save_Plots)
 os.system("rm -r "+ Directory_Save_Results)
 os.system("mkdir "+ Directory_Save_Results)
+os.system("sudo rm perf.data*")
 
 import random
 plt.close('all')
@@ -31,6 +32,3 @@ print Avg_Events
 Plot_All_Parameters(Avg,1,Avg_Events)
 
 os.system("cp "+BENCH_DIR + " " + Directory_Save_Results )#save the obj file for the future!
-#os.system("sudo rm *.txt")
-os.system("sudo rm Avg_Results")
-os.system("sudo rm perf.data*")

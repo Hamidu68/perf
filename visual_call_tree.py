ee6490e16cdce53d6899a6448be66e1c124aa0fb
hamid <hnejatol@uci.edu>
2018-06-12 14:57:14 -0700

    add caption to the call graph

--- visual_call_tree.py
+++ visual_call_tree.py
@@ -1,22 +1,20 @@
 from graphviz import Digraph
 
-# call_tree = {'a':[['b',1],['c',2],['d',4],['g',5]],
-#              'b':[['g',6]],
-#              'c':[['e',7],['f',8]],
-#              'e':[['g',9]]}
-
-
 def vis_call_graph(call_tree, fname, threshold):
     f = Digraph('unix', filename=fname)
-    f.attr(size='2,2')
     f.node_attr.update(color='lightblue2', style='filled')
+    f.attr(label="text inside each node: [function name] ([inclusive percentage], [exclusive percentage]]", size='2,2')
 
     for caller in call_tree: #key is the caller function
         for callee in caller.kids:
             if float(callee.percent[:-1]) > threshold:
                 f.edge(caller.name,callee.name,callee.percent)
 
- #   f.view()
+ #  f.view()
     f.render()
 
+# call_tree = {'a':[['b',1],['c',2],['d',4],['g',5]],
+#              'b':[['g',6]],
+#              'c':[['e',7],['f',8]],
+#              'e':[['g',9]]}
 #vis_call_graph(call_tree, 'test')
